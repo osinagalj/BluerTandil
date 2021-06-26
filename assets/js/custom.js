7e566f0e488d4c22f49ad7dd0651982a428b8188
Lautaro Osinaga <49956395+osinagalj@users.noreply.github.com>
2021-06-26 19:32:38 -0400

    insta link

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,3 +13,16 @@ $('a.remove').click(function(){
 var month = (new Date()).getMonth();
 var myURL = 'https://api.whatsapp.com/send?phone=542235386914&text=Me%20gustar%C3%ADa%20saber%20el%20precio%20de%20los%20perfumes%20para%20auto'; //+ month + '&param=1';
 document.getElementById('myUniqueLinkId').href = myURL;
+
+
+
+function miFuncion()
+{   
+    
+    product = document.getElementById('ProducName').innerHTML;
+    price = document.getElementById('Price').innerHTML;
+    alert("producto = " + product + " precio = " + price);
+}
+
+
+
